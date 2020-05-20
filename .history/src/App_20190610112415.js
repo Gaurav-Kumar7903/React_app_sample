dcb90b37f1f3a5ff3e77d01c9317d8271472b2aa
Gaurav Kumar <thakurgaurav7903@gmail.com>
2020-05-20 17:11:08 +0530

    sample react app

---
+++ .history/src/App_20190610112415.js
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App">hello from sample app</div>;
+}
+
+export default App;
